3ca92adb874f8f2d0c826ae72f75fb4b172ebd49
gaoguhao <gaoguhao@126.com>
2022-09-23 02:26:29 +0800

    no message

---
+++ sharewar2/pages/index/index.vue
@@ -0,0 +1,51 @@
+<template>
+	<Header></Header>
+</template>
+
+<script>
+	import Header from '@/components/header/header.vue';
+	export default {
+		components:{
+			Header
+		},
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
